dfeaa9165f2fa0823a41e92c757f176d326929b1
songxiren991120 <78586308+songxiren991120@users.noreply.github.com>
2021-03-03 16:23:05 +0800

    实现命令行读取问价
    实现命令行读取问价

---
+++ src/WordCount.cpp
@@ -0,0 +1,18 @@
+﻿#include <vector>
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string address = argv[1];
+    ifstream ifile(address);
+    string s = "";
+    while (getline(ifile, s)) {
+        cout << s << endl;
+    }
+}
+
