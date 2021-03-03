3224c6db6efad76fa6b57639ff769c69677f4dc2
songxiren991120 <78586308+songxiren991120@users.noreply.github.com>
2021-03-03 16:59:35 +0800

    实现单词统计

--- src/WordCount.cpp
+++ src/WordCount.cpp
@@ -5,7 +5,33 @@
 #include <sstream>
 #include <iostream>
 using namespace std;
+int wordtest(char a)
+{
+    return ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z') || (a >= '0' && a <= '9'));
+}
+void readText(const char* filename) {
+    ifstream ifile(filename);
+    char para[100];
+    int wordcount = 0;
+    while (!ifile.eof()) {
+        ifile.getline(para, 99);
+        int i = 0;
+        while (para[i]!='\0') {
+            i++;
+            while (wordtest(para[i]))
+            {
+                i++;
+                if (!wordtest(para[i]))
+                {
+                    wordcount++;
+                }
+            }
+        }
 
+    }
+    ifile.close();
+    cout << "单词：" << wordcount << endl;
+}
 int main(int argc, char** argv)
 {
     string address = argv[1];
@@ -14,5 +40,6 @@ int main(int argc, char** argv)
     while (getline(ifile, s)) {
         cout << s << endl;
     }
+    const char* address2 = address.data();
+    readText(address2);
 }
-
